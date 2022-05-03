14046a0bbb84d8f23155a37105e4ebfed008f1a9
anand-patel05 <94690359+anand-patel05@users.noreply.github.com>
2022-05-03 10:02:22 +0530

    Update rough.c

--- rough.c
+++ rough.c
@@ -5,14 +5,14 @@ void main()
 {
 	int i,j,keylen,datalen;
 	char data[100], key[30],temp[30],rem[30],key1[30], Endata[100];
-	// clrscr();
+
+	//Sender's Side
 	printf("Enter Data: ");         // taking input
 	gets(data);
 	printf("Enter Key: ");
 	gets(key);
 	keylen=strlen(key);
 	datalen=strlen(data);          // finding length
-	//strcpy(key1,key);               // remember
 
 	for (i=0;i<keylen-1;i++)
     {
@@ -20,24 +20,20 @@ void main()
 	}
 
 	strcpy(key1,key); 
-	for(i=0;i<keylen;i++)            
+	for(i=0;i<keylen;i++)             
 	    temp[i]=data[i];
     
-	for (i=0;i<datalen;i++)        //
+	for (i=0;i<datalen;i++)         //---------------------------------        
     {
-	     //quot[i]=temp[0];
-	      if(temp[0] == '0')
-		  {
-	 	  for (j=0;j<keylen;j++)
-	 	      key[j]='0'; 
-		  }
-          else
-		  {
-	 		for (j=0;j<keylen;j++)         // Logic of program
-	 	    	key[j]=key1[j];
-		  }
+		for (j=0;j<keylen;j++)
+		 {
+			if (temp[0] == '0')
+	 	       	key[j]='0'; 
+			else
+				key[j]=key1[j];
+		}
 
-	 	for (j=keylen-1;j>0;j--)
+	 	for (j=keylen-1;j>0;j--)      // Logic of program
          {
 	 		if(temp[j]==key[j])
 	 		    rem[j-1]='0';
@@ -49,10 +45,7 @@ void main()
 	 	strcpy(temp,rem);
 	 }
 
-	  strcpy(rem,temp);                   //
-	//   printf("\nQuotient is ");
-	//   for (i=0;i<msglen;i++) 
-	//    printf("%c",quot[i]);
+	  strcpy(rem,temp);               //-----------------------------------
 
 	printf("\nRemainder at sender's side: ");
 	for (i=0;i<keylen-1;i++)
@@ -69,61 +62,53 @@ void main()
         Endata[c++] = rem[i];
 	    printf("%c",rem[i]);
     }
-    // printf("\nEncoded data: ");
-	// for (i=0;i<c;i++)
-	// printf("%c", Endata[i]);
 
-    //Endata[2] = 1;                                  // *** This is for checking tranmission of corrupted data *** 
+    //Endata[3] = 1;                                  // *** This is for checking tranmission of corrupted data *** 
 
-    for(i=0;i<keylen;i++)
+	// Reciever's Side
+	for(i=0;i<keylen;i++)            
 	    temp[i]=Endata[i];
-
-
-    for (i=0;i<datalen;i++)
+    
+	for (i=0;i<datalen;i++)            //
     {
-	    //quot[i]=temp[0];
-	    if(temp[0]=='0')
-		{
-	 	for (j=0;j<keylen;j++)
-	 	    key[j]='0'; 
+		for (j=0;j<keylen;j++)
+		 {
+			if (temp[0] == '0')
+	 	       	key[j]='0'; 
+			else
+				key[j]=key1[j];
 		}
-        else
-		{
-	 	for (j=0;j<keylen;j++)
-	 	    key[j]=key1[j];
-		}
-	 	for (j=keylen-1;j>0;j--)
+
+	 	for (j=keylen-1;j>0;j--)      // Logic of program
          {
 	 		if(temp[j]==key[j])
 	 		    rem[j-1]='0';
             else
 	 		    rem[j-1]='1';
 	 	 }
+		 
 	 	rem[keylen-1]=Endata[i+keylen];
 	 	strcpy(temp,rem);
 	 }
 
-	  strcpy(rem,temp);
-
-    int remlen = strlen(rem);
+	strcpy(rem,temp);                   //
 
     printf("\nRemainder at reciever's side: ");
 	for (i=0;i<keylen-1;i++)
-	printf("%c",rem[i]);
+		printf("%c",rem[i]);
 
-	for (i=0;i<remlen;i++)
-    {
-	if (rem[i] == '1')
-    {
-        printf("\n\nError in data transmisssion!");
-        break;
-    }
-    else
-    {
-        printf("\n\nNo Error in data transmisssion!");
-        break;
-    }
-    }
+	int flag = 0;
+	for (i=0;i<keylen-1;i++)
+     {
+	 if (rem[i] == '1')
+		flag = 1; 
+     else
+		flag = 0;
+     }
+	 if (flag == 1)
+	 	printf("\n\nError in data transmisssion!");
+	 else
+	 	printf("\n\nNo Error in data transmisssion!");
     
 	getch();
 }
